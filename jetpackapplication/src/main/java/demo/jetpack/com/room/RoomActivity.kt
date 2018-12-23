bda56c859ce12eb51c5581ef1a39e72ad7a3c778
kuanghaochuan <609779612@qq.com>
2018-12-23 10:42:24 +0800

    add Room

--- jetpackapplication/src/main/java/demo/jetpack/com/room/RoomActivity.kt
+++ jetpackapplication/src/main/java/demo/jetpack/com/room/RoomActivity.kt
@@ -2,21 +2,41 @@ package demo.jetpack.com.room
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.idescout.sql.SqlScoutServer
 import demo.jetpack.com.JetpackApplication
 import demo.jetpack.com.R
 import kotlinx.android.synthetic.main.activity_room.*
 import kotlinx.coroutines.*
 
 class RoomActivity : AppCompatActivity() {
-
-    val job = Job()
-    val uiScope = CoroutineScope(Dispatchers.Main + job)
+    lateinit var sqlScoutServer: SqlScoutServer
+    val job: Job? = Job()
+    val uiScope = CoroutineScope(Dispatchers.Main + job!!)
+    val ioScope = CoroutineScope(Dispatchers.IO + job!!)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_room)
 
-        activity_room_insert.setOnClickListener {
+        sqlScoutServer = SqlScoutServer.create(this, packageName)
+
+        activity_room_query.setOnClickListener {
+            ioScope.launch {
+                val lists = queryAllUsers()
+
+                val sb: StringBuilder = StringBuilder()
+
+                lists.forEach { UserEntity ->
+                    sb.append(UserEntity.uid.toString() + UserEntity.firstName + UserEntity.lastName).append("\n")
+                }
+                withContext(Dispatchers.Main) {
+                    activity_room_tv.text = ""
+                    activity_room_tv.text = sb.toString()
+                }
+            }
+        }
+
+        activity_room_add.setOnClickListener {
             // Cannot access database on the main thread since it may potentially lock the UI for a long period of time.
             // 这里不能在UI线程进行数据库的操作
             uiScope.launch {
@@ -31,6 +51,7 @@ class RoomActivity : AppCompatActivity() {
                 lists.forEach {
                     sb.append(it.uid.toString() + it.firstName + it.lastName).append("\n")
                 }
+                activity_room_tv.text = ""
                 activity_room_tv.text = sb.toString()
             }
         }
@@ -50,5 +71,12 @@ class RoomActivity : AppCompatActivity() {
             .getUserDatabase()
             .userDao()
             .queryAllUsers()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        job?.let {
+            it.cancel()
+        }
     }
 }
