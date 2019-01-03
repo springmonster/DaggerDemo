75eec3c86745a580a90a48e63a7980802d371b93
Charles Kuang <charles.kuang@homecreditcfc.cn>
2019-01-03 16:08:14 +0800

    modify download animation

--- jetpackapplication/src/main/java/demo/jetpack/com/downloadmanager/DownloadManagerActivity.kt
+++ jetpackapplication/src/main/java/demo/jetpack/com/downloadmanager/DownloadManagerActivity.kt
@@ -1,13 +1,17 @@
 package demo.jetpack.com.downloadmanager
 
 import android.os.Bundle
-import android.widget.Button
+import android.os.Handler
+import android.os.Message
 import androidx.appcompat.app.AppCompatActivity
 import demo.jetpack.com.R
+import kotlinx.android.synthetic.main.activity_download_manager.*
 
 
 class DownloadManagerActivity : AppCompatActivity() {
     var progressInt = 0
+    var myHandler: MyHandler = MyHandler()
+    lateinit var downloadProgressView: DownloadProgressBar
 
     companion object {
         private const val REQUEST_CODE_GET_IMAGE = 1
@@ -17,16 +21,47 @@ class DownloadManagerActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_download_manager)
 
+        downloadProgressView = findViewById(R.id.dpv3)
 
-        val downloadProgressView = findViewById<DownloadProgressBar>(R.id.dpv3)
-        val successBtn = findViewById<Button>(R.id.success_text_btn)
-        successBtn.setOnClickListener {
-            progressInt += 10
-            downloadProgressView.setProgress(progressInt)
+        download_btn_start.setOnClickListener {
+            val msg = Message()
+            msg.what = 1
+            myHandler.sendMessage(msg)
         }
 
-        downloadProgressView.setOnClickListener {
-            downloadProgressView.playManualProgressAnimation()
+        download_btn_end.setOnClickListener {
+            myHandler.sendMessage(Message.obtain(myHandler, 3))
+        }
+    }
+
+    inner class MyHandler : Handler() {
+        override fun handleMessage(msg: Message) {
+            when (msg.what) {
+                1 -> {
+                    downloadProgressView.startLoading()
+                    val msg = Message()
+                    msg.what = 2
+                    myHandler.sendMessage(msg)
+                }
+                2 -> {
+                    progressInt += 10
+
+                    if (progressInt > 100) {
+                        val msg = Message()
+                        msg.what = 3
+                        myHandler.sendMessage(msg)
+                    } else {
+                        val msg = Message()
+                        msg.what = 2
+                        myHandler.sendMessageDelayed(msg, 300)
+                        downloadProgressView.setProgress(progressInt)
+                    }
+                }
+                3 -> {
+                    progressInt = 0
+                    downloadProgressView.stopLoading()
+                }
+            }
         }
     }
 }
---
+++ jetpackapplication/src/main/java/demo/jetpack/com/downloadmanager/DownloadProgressBar.kt
@@ -0,0 +1,163 @@
+package demo.jetpack.com.downloadmanager
+
+import android.animation.ValueAnimator
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.RectF
+import android.util.AttributeSet
+import android.view.View
+import demo.jetpack.com.R
+
+class DownloadProgressBar : View {
+    private lateinit var mBackgroundPaint: Paint
+    private lateinit var mDrawingPaint: Paint
+
+    private var mCircleRadius: Float = 0.toFloat()
+    private var mStrokeWidth: Float = 0.toFloat()
+    private var mLengthFix: Float = 0.toFloat()
+
+    private var mCenterX: Float = 0.toFloat()
+    private var mCenterY: Float = 0.toFloat()
+    private var mPaddingX: Float = 0.toFloat()
+    private var mPaddingY: Float = 0.toFloat()
+
+    private var mBackgroundColor: Int = 0
+    private var mDrawingColor: Int = 0
+
+    private lateinit var mLoadingAnimation: ValueAnimator
+
+    private lateinit var mCircleBounds: RectF
+
+    private var mFromArc = 0f
+    private var mToArc = 0f
+    private var mCurrentProgressValue: Float = 0.toFloat()
+
+    private var mState = State.IDLE
+
+    private enum class State {
+        IDLE, LOADING
+    }
+
+    constructor(context: Context) : super(context)
+
+    constructor(context: Context, attrs: AttributeSet) : super(context, attrs) {
+        initAttrs(context, attrs)
+        init()
+    }
+
+    private fun initAttrs(context: Context, attrs: AttributeSet) {
+        val array = context.obtainStyledAttributes(attrs, R.styleable.DownloadProgressView, 0, 0)
+        try {
+            mCircleRadius = array.getDimension(R.styleable.DownloadProgressView_circleRadius, 0f)
+            mStrokeWidth = array.getDimension(R.styleable.DownloadProgressView_strokeWidth, 0f)
+            mLengthFix = (mStrokeWidth / (2 * Math.sqrt(2.0))).toFloat()
+            mBackgroundColor = array.getColor(R.styleable.DownloadProgressView_backgroundColor, 0)
+            mDrawingColor = array.getColor(R.styleable.DownloadProgressView_drawingColor, 0)
+        } finally {
+            array.recycle()
+        }
+    }
+
+    private fun init() {
+        mBackgroundPaint = Paint()
+        mBackgroundPaint.flags = Paint.ANTI_ALIAS_FLAG
+        mBackgroundPaint.style = Paint.Style.STROKE
+        mBackgroundPaint.color = mBackgroundColor
+        mBackgroundPaint.strokeWidth = mStrokeWidth
+
+        mDrawingPaint = Paint()
+        mDrawingPaint.flags = Paint.ANTI_ALIAS_FLAG
+        mDrawingPaint.style = Paint.Style.STROKE
+        mDrawingPaint.color = mDrawingColor
+        mDrawingPaint.strokeWidth = mStrokeWidth
+
+        setupAnimations()
+    }
+
+    override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
+        super.onSizeChanged(w, h, oldw, oldh)
+
+        mCenterX = w / 2f
+        mCenterY = h / 2f
+        mPaddingX = w / 2f - mCircleRadius
+        mPaddingY = h / 2f - mCircleRadius
+
+        mCircleBounds = RectF()
+        mCircleBounds.top = mPaddingY
+        mCircleBounds.left = mPaddingX
+        mCircleBounds.bottom = h / 2f + mCircleRadius
+        mCircleBounds.right = w / 2f + mCircleRadius
+    }
+
+    private fun setupAnimations() {
+        mLoadingAnimation = ValueAnimator.ofFloat(mFromArc, mToArc)
+        mLoadingAnimation.addUpdateListener { valueAnimator ->
+            mCurrentProgressValue = valueAnimator.animatedValue as Float
+            invalidate()
+        }
+        mLoadingAnimation.start()
+    }
+
+    private fun drawArrow(canvas: Canvas, drawingPaint: Paint) {
+        canvas.drawLine(mCenterX, mCenterY - mCircleRadius / 2, mCenterX, mCenterY + mCircleRadius / 2, drawingPaint)
+
+        canvas.drawLine(
+            mCenterX - mCircleRadius / 2,
+            mCenterY,
+            mCenterX + mLengthFix,
+            mCenterY + mCircleRadius / 2 + mLengthFix,
+            drawingPaint
+        )
+
+        canvas.drawLine(
+            mCenterX - mLengthFix,
+            mCenterY + mCircleRadius / 2 + mLengthFix,
+            mCenterX + mCircleRadius / 2,
+            mCenterY,
+            drawingPaint
+        )
+    }
+
+    override fun onDraw(canvas: Canvas) {
+        super.onDraw(canvas)
+        drawing(canvas)
+    }
+
+    private fun drawing(canvas: Canvas) {
+        canvas.drawCircle(mCenterX, mCenterY, mCircleRadius, mBackgroundPaint)
+
+        when (mState) {
+            State.IDLE -> {
+                drawArrow(canvas, mBackgroundPaint)
+            }
+            State.LOADING -> {
+                canvas.drawArc(mCircleBounds, -90f, mCurrentProgressValue, false, mDrawingPaint)
+                drawArrow(canvas, mDrawingPaint)
+            }
+        }
+    }
+
+    fun startLoading() {
+        mState = State.LOADING
+        invalidate()
+    }
+
+    fun setProgress(value: Int) {
+        if (value < 0 || value > 100) {
+            return
+        }
+        mToArc = value * 3.6f
+        mLoadingAnimation.setFloatValues(mFromArc, mToArc)
+        mLoadingAnimation.start()
+        mFromArc = mToArc
+        invalidate()
+    }
+
+    fun stopLoading() {
+        mFromArc = 0f
+        mToArc = 0f
+        mState = State.IDLE
+        invalidate()
+    }
+}
