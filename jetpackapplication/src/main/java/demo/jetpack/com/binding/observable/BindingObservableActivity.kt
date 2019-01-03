99a72c3e5fd9be87f2f8c7a88746f8c2c60a9f0a
Charles Kuang <charles.kuang@homecreditcfc.cn>
2019-01-03 13:34:13 +0800

    modify databinding

--- jetpackapplication/src/main/java/demo/jetpack/com/binding/observable/BindingObservableActivity.kt
+++ jetpackapplication/src/main/java/demo/jetpack/com/binding/observable/BindingObservableActivity.kt
@@ -6,8 +6,11 @@ import android.text.TextWatcher
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.databinding.ObservableArrayMap
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Observer
 import demo.jetpack.com.R
 import demo.jetpack.com.databinding.ActivityBindingObservableBinding
+import kotlinx.android.synthetic.main.activity_binding_observable.*
 
 class BindingObservableActivity : AppCompatActivity() {
 
@@ -40,5 +43,28 @@ class BindingObservableActivity : AppCompatActivity() {
             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
             }
         })
+
+        val text = MutableLiveData<String>()
+        text.value = "hello world"
+
+        val text1 = MutableLiveData<String>()
+        text1.value = "hello world"
+
+        val text2 = MutableLiveData<String>()
+        text2.value = "hello world"
+
+        val text3 = MutableLiveData<String>()
+        text3.value = "hello world"
+
+        binding.bindingPerson = BindingPerson(text, text1, text2, text3)
+
+        binding.setLifecycleOwner(this)
+
+        binding.bindingPerson?.text?.observe(this, Observer {
+        })
+        
+        activity_binding_btn.setOnClickListener {
+            binding.bindingPerson?.text?.value = "hello world 1"
+        }
     }
 }
---
+++ jetpackapplication/src/main/java/demo/jetpack/com/binding/observable/BindingPerson.kt
@@ -0,0 +1,14 @@
+package demo.jetpack.com.binding.observable
+
+import androidx.lifecycle.MutableLiveData
+
+/**
+ * @author Charles.Kuang
+ */
+data class BindingPerson(
+    val text: MutableLiveData<String>,
+    val text1: MutableLiveData<String>,
+    val text2: MutableLiveData<String>,
+    val text3: MutableLiveData<String>
+) {
+}
